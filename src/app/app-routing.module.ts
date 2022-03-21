2bb01fd57e9959b82fb99d2efdcb2f102b04160e
Vilia <vzeisig@gmail.com>
2022-03-21 15:46:34 +0100

    routing to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginPageComponent },
 
 ];
 
