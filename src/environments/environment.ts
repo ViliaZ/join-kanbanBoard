03ebdbb531257f319b0c8ccf728e5c2328ffda0e
Vilia <vzeisig@gmail.com>
2022-03-21 19:27:46 +0100

    setup firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCvFXgeAf_CZWY3XVHhv7hIR7yz-IGOGoU",
+    authDomain: "join-6d44b.firebaseapp.com",
+    databaseURL: "https://join-6d44b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "join-6d44b",
+    storageBucket: "join-6d44b.appspot.com",
+    messagingSenderId: "919315633017",
+    appId: "1:919315633017:web:590623c99e60e8fa9515a8"
+  }
 };
 
 /*
