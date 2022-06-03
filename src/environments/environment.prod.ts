6686c7e02897968d796833b5ebb2e87cc1d8c6ac
Vilia <vzeisig@gmail.com>
2022-06-03 11:25:34 +0200

    adapted env files for prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCvFXgeAf_CZWY3XVHhv7hIR7yz-IGOGoU",
+    authDomain: "join-6d44b.firebaseapp.com",
+    databaseURL: "https://join-6d44b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "join-6d44b",
+    storageBucket: "join-6d44b.appspot.com",
+    messagingSenderId: "919315633017",
+    appId: "1:919315633017:web:590623c99e60e8fa9515a8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase:{
     apiKey: "AIzaSyCvFXgeAf_CZWY3XVHhv7hIR7yz-IGOGoU",
     authDomain: "join-6d44b.firebaseapp.com",
