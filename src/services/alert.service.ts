7aa45358f485e1d57d009a07d8f3ee552d163463
Vilia <vzeisig@gmail.com>
2022-06-08 13:26:27 +0200

    make alerts responsive

--- src/services/alert.service.ts
+++ src/services/alert.service.ts
@@ -6,14 +6,14 @@ import { DatabaseService } from './database.service';
 })
 export class AlertService {
 
-  alertStatusON: boolean = false; // toggles alerts open, for setTimeout() 
+  alertStatusON: boolean = true; // toggles alerts open, for setTimeout() 
   indexOfBoardToDelete!: number;
 
   alerts: any = {  
     "backlogAlert": false,  // 
     "editsProhibited": false, // no Edits on Todo board
     "duplicateAlert": false,
-    "confirmDeleteBoard": false,
+    "confirmDeleteBoard": true,
     "confirmEditTask": false,
     "confirmAddTask": false,
     "confirmDeleteTask": false,
